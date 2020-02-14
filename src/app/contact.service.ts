30a08fe3fffe4ce687bc459c7ce5de000cf45483
ezioguga <ezioguga@gmail.com>
2020-02-14 19:27:20 +0530

    my first commit

---
+++ src/app/contact.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Contact } from './contact';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  private _user: Contact;
+  private baseUrl = 'http://localhost:3000/api';
+
+  constructor(private _http: HttpClient, private header: HttpHeaders) {
+  }
+
+  // retrieving ContactServices
+
+  getContacts(): Observable<any> {
+
+    return this._http.get<Contact>(this.baseUrl + '/contacts');
+  }
+}
---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from './../contact.service';
+import {Contact} from '../contact';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
+})
+export class ContactsComponent implements OnInit {
+  contacts: Contact[];
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.contactService.getContacts().subscribe(contacts => this.contacts = contacts);
+  }
+
+}
